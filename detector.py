08859c0c349355c832c9d283066d2f0855ccd13b
Tim <timwagner997@gmail.com>
2023-10-17 13:42:30 +0200

    created a webcam image for simulation

--- detector.py
+++ detector.py
@@ -1,7 +1,36 @@
 import numpy as np
 import cv2 as cv
-im = cv.imread('uno cards.jpeg')
-assert im is not None, "file could not be read, check with os.path.exists()"
-imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 127, 255, 0)
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+img = cv.imread('webcam simulation.jpeg')
+
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+ret, thresh = cv.threshold(
+    src=img_gray,
+    thresh=130,
+    maxval=255,
+    type=cv.THRESH_BINARY
+)
+
+cv.imshow('thresh', thresh)
+cv.waitKey(0)
+
+contours, hierarchy = cv.findContours(
+    image=thresh,
+    mode=cv.RETR_TREE,
+    method=cv.CHAIN_APPROX_NONE
+)
+
+img_with_contours = img.copy()
+
+cv.drawContours(
+    image=img_with_contours,
+    contours=contours,
+    contourIdx=-1,
+    color=(0,255,0),
+    thickness=2
+)
+
+cv.imshow('contours', img_with_contours)
+cv.waitKey(0)
+cv.destroyAllWindows()
