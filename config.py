12dd0325ba5476babfed5c7bb62e313efe2a0d65
Tim <timwagner997@gmail.com>
2023-10-17 21:18:46 +0200

    put config values into a new file

---
+++ config.py
@@ -0,0 +1,2 @@
+area_filter_size = 50_000
+gray_image_thresh = 130
--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
-import numpy as np
 import cv2 as cv
+
+import config
 
 img = cv.imread('webcam simulation.jpeg')
 
@@ -7,7 +8,7 @@ img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 ret, thresh = cv.threshold(
     src=img_gray,
-    thresh=130,
+    thresh=config.gray_image_thresh,
     maxval=255,
     type=cv.THRESH_BINARY
 )
@@ -18,18 +19,17 @@ contours, hierarchy = cv.findContours(
     method=cv.CHAIN_APPROX_NONE
 )
 
-def is_bigger_contour(contour):
-    area = cv.contourArea(contour)
-    return area > 50_000
+
+def is_bigger_contour(contour_to_check):
+    area = cv.contourArea(contour_to_check)
+    return area > config.area_filter_size
+
 
 bigger_contours_iterator = filter(is_bigger_contour, contours)
 bigger_contours = list(bigger_contours_iterator)
 
-
 for contour in contours:
     print(cv.contourArea(contour))
-
-
 
 img_with_contours = img.copy()
 
@@ -37,7 +37,7 @@ cv.drawContours(
     image=img_with_contours,
     contours=bigger_contours,
     contourIdx=-1,
-    color=(0,255,0),
+    color=(0, 255, 0),
     thickness=2
 )
 
