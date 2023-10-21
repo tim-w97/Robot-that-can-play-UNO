caa93fdc8a63435379623db837c9f8ff60c4f25b
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2023-10-21 14:44:26 +0200

    tested game loop

--- game_test.py
+++ game_test.py
@@ -1,29 +1,42 @@
 from game_logic import *
 import keyboard
 
+#This class is just for testing the game logic itself and the classes and methodes inside game_logic.py#
+
 class GameTest:
 
     def __init__(self,player_one,player_two,main_stack):
         self.player_one = player_one
         self.player_two = player_two
         self.main_stack = main_stack
+        self.game_is_over = False
     
-    def test_func(self):
+    def test_func_remove_add(self):
         card_stack_p1 = CardStack([UnoCard(1,Color.RED),UnoCard(7,Color.GREEN),UnoCard(5,Color.BLUE)])
         p1 = Player("John Doe",card_stack_p1)
         p1.add_card(UnoCard(2,Color.YELLOW))
         p1.remove_card(UnoCard(2,Color.YELLOW))
         print(f'Card count of player {p1.name} = {p1.get_card_count()}.')
 
-    def run_game(self):
-        while(not keyboard.is_pressed('q')):
-            pass
+    def set_winner(self,player):
+        if player.get_card_count() == 0:
+            print(f'{player.name} has won the game!')
+            self.game_is_over = True
+        else:
+            self.game_is_over = False
 
-player_1 = Player("John Doe",CardStack([UnoCard(1,Color.RED),UnoCard(7,Color.GREEN),UnoCard(5,Color.BLUE)]))
+    def run_game(self):
+        while(not keyboard.is_pressed('q') and not self.game_is_over):
+            self.player_one.play_card(UnoCard(1,Color.RED))
+            print(self.player_one.get_card_count())
+            print(f'gamestate:{self.game_is_over}')
+            self.set_winner(self.player_one)
+
+
+
+player_1 = Player("John Doe",CardStack([UnoCard(1,Color.RED)]))
 player_2 = Player("Jane Doe",CardStack([UnoCard(1,Color.RED),UnoCard(7,Color.GREEN),UnoCard(5,Color.BLUE)]))
 main_stack = CardStack([UnoCard(1,Color.RED)])
 test_game = GameTest(player_1,player_2,main_stack)
-
-
-print(player_1.play_card(UnoCard(1,Color.RED)).get_color())
-#test_game.run_game()
+test_game.run_game()
+test_game.test_func_remove_add()
