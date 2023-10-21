c7572173b089471b7d2571350b63383a3f6f1871
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2023-10-21 10:59:22 +0200

    created Cardstack class

--- gamelogic.py
+++ gamelogic.py
@@ -18,41 +18,58 @@ class UnoCard:
     
     def get_color(self)->Color:
         return self.color
+    
+class CardStack:
 
-class Player:
-
-    def __init__(self,name,cards):
-        self.player = name
+    def __init__(self,cards):
         self.cards = cards
-        self.card_amount = len(self.cards)
+        self.card_amount = len(cards)
 
     def add_card(self,card):
         self.cards.append(card)
         self.card_amount = len(self.cards)
-    
+
     def remove_card(self,card):
         for i in self.cards:
                 if i.number == card.number and i.color == card.color:
                     self.cards.remove(i)
         self.card_amount = len(self.cards)
 
-class Game:
-    #if color or number equal...
+class Player:
+
+    def __init__(self,name,cardstack):
+        self.player = name
+        self.cardstack = cardstack
+
+    def add_card(self,card):
+        self.cardstack.add_card(card)
+    
+    def remove_card(self,card):
+        self.cardstack.remove_card(card)
+    
+    def get_card_count(self) -> int:
+        return self.cardstack.card_amount
+#maybe add a Cardstack class
+
+#if color or number equal...
     #user input move finished...
     #for testing console input/output...
     #two card stacks one for drawing one for field...
     #state...
+
+class Game:
+    
     pass
 
 class GameTest:
     pass
 
 def test_func():
-    cards = [UnoCard(1,Color.RED),UnoCard(7,Color.GREEN),UnoCard(5,Color.BLUE)]
-    p1 = Player("John doe",cards)
+    card_stack_p1 = CardStack([UnoCard(1,Color.RED),UnoCard(7,Color.GREEN),UnoCard(5,Color.BLUE)])
+    p1 = Player("John doe",card_stack_p1)
     p1.add_card(UnoCard(2,Color.YELLOW))
     p1.remove_card(UnoCard(2,Color.YELLOW))
-    print(p1.card_amount)
+    print(p1.get_card_count())
 
 test_func()
 
