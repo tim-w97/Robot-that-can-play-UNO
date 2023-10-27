1d63a81e8dbafa2d82d788434ec3e72652214a7c
Tim <timwagner997@gmail.com>
2023-10-27 12:04:27 +0200

    refactor

--- card_cropper.py
+++ card_cropper.py
@@ -21,12 +21,18 @@ width = int(rect[1][0])
 height = int(rect[1][1])
 
 src_pts = box.astype("float32")
+
 # coordinate of the points in box points after the rectangle has been
 # straightened
-dst_pts = np.array([[0, height-1],
-                    [0, 0],
-                    [width-1, 0],
-                    [width-1, height-1]], dtype="float32")
+dst_pts = np.array(
+    object=[
+        [0, height-1],
+        [0, 0],
+        [width-1, 0],
+        [width-1, height-1]
+    ],
+    dtype="float32"
+)
 
 # the perspective transformation matrix
 M = cv.getPerspectiveTransform(src_pts, dst_pts)
