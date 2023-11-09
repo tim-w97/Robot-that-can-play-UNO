321a8d32e0442319af28b6ad8c6fc64181412d11
Tim <timwagner997@gmail.com>
2023-11-09 16:02:04 +0100

    created a basic pick up card and release program

--- robot/pickup_card.py
+++ robot/pickup_card.py
@@ -1,5 +1,3 @@
-import math
-
 from control import RobotProxy
 
 print("init")
@@ -7,13 +5,36 @@ print("init")
 proxy = RobotProxy()
 proxy.connect()
 
-proxy.move(z=0.1)
 
-proxy.grab()
+# move the arm to the position where the robot should pick up the uno card
+def move_to_pick_up_pos():
+    proxy.move(
+        x=-0.002,
+        y=0.269,
+        z=0.094,
+        roll=-1.648,
+        pitch=0.155,
+        yaw=1.701
+    )
 
-proxy.move(x=0, y=0.4)
 
-proxy.release()
+# move the arm to the position where robot should release the uno card
+def move_to_release_pos():
+    proxy.move(
+        x=0.251,
+        y=-0.041,
+        z=0.073,
+        roll=1.517,
+        pitch=0.544,
+        yaw=-0.006
+    )
+
+
+for i in range(3):
+    move_to_pick_up_pos()
+    proxy.grab()
+    move_to_release_pos()
+    proxy.release()
 
 proxy.disconnect()
 
