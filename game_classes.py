14a085278ca1dfe3c3d7bd098e916753523e058c
Lukas Drescher <lukas.drescher@hof-university.de>
2023-11-10 20:01:38 +0100

    Added the game logic and tests

--- game_classes.py
+++ game_classes.py
@@ -20,10 +20,10 @@ class UnoCard:
     def get_color(self) -> Color:
         return self.color
     
-    def to_string(self) -> str:
+    def __str__(self) -> str:
         return ''.join(f'{str(self.color)} {str(self.number)}')
 
-    def match(self, other: UnoCard):
+    def match(self, other):
         return self.color == other.color or self.number == other.number
     
 class CardStack:
@@ -62,16 +62,19 @@ class CardStack:
     def cards_to_string(self) -> str:
         return ' '.join(f'{card.to_string()}'for card in self.get_all_cards())
 
-#TODO: Think about the member functions again. A human player don't need to control a stack. A robot does.
-# The game does not have to differ between players. But the each player has to signal the game, that they are done
-# with their turn.
 """
-This class represents a player.
+This class represents a player. The game does not distinguish between a human and a robot.
+A Human Player has to type in the number of his cards. He doesn't need a virtual card deck.
+A robot need a virtual card stack and a physical one. He has to do some further steps.
 """
 class Player:
 
     def __init__(self, name: str):
         self.name = name
+        self.card_amount = 7
+
+    def get_card_count(self) -> int:
+        return self.card_amount
     
     """
     This method is called from the game and needs to be overwritten.
@@ -79,60 +82,62 @@ class Player:
     def doMove(self) -> bool:
         pass
 
+    def __str__(self):
+        return self.name
+
 class HumanPlayer(Player):
     def __init__(self, name: str):
-        super(name)
+        super(HumanPlayer, self).__init__(name)
 
     """
-    Just wait for a signal.
+    Waits on the player to type in the number of cards he has.
     """
-    @Overwrite
     def doMove(self) -> bool:
-        pass
+        self.card_amount = int(input(f"It's your turn {self.name}. How many cards do you have?"))
 
 
 """
 This is the class to control the game. It is responsible for a game flow.
+How to use Game:
+1. Create an initial unocard
+2. Create an instance
+3. Call run_game
 """
 class Game:
 
-    def __init__(self, *args: [Player]):
+    def __init__(self, *args: [Player], initialCard: UnoCard):
         if len(args) < 2: raise Exception("You need at least two players!")
         self.players = args
-        self.activePlayer = args[0]
+        self.activePlayer = -1
+        self.activeCard = initialCard
 
-    # Refactored this method in UnoCard
-    def match_cards(self, card_1: UnoCard, card_2: UnoCard) -> bool:
-        if(card_1.color == card_2.color or card_1.number == card_2.number):
-            return True
-        return False
-    
-    def check_winner(self,player)->bool:
-        if player.get_card_count() == 0:
-            print(f'{player.name} has won the game!')
-            self.game_is_over = True
-            return self.game_is_over
+    def get_next_player(self) -> Player:
+        if self.activePlayer == len(self.players) - 1:
+            self.activePlayer = 0
         else:
-            self.game_is_over = False
-            return self.game_is_over
+            self.activePlayer += 1
+        return self.players[self.activePlayer]
+    
+    def is_active_player_winning(self) -> bool:
+        if self.activePlayer == -1: return False
 
+        player = self.players[self.activePlayer]
 
-    def switch_current_player(self,player):
-        self.current_player = player
+        return player.get_card_count() == 0
 
-    def turn(self,player):
-        #TODO
-        #1. Display current players card
-        print(player.cards_to_string)
-        #2. Player Input
-        #3. Compare there cards with main stack or moved pulled card to player stack
-        #4. check if game is over
-        #5. if not over switch players
+    """
+    This method analyze the main deck by card detection.
+    """
+    def update_game_stats(self):
+        print("updated")
 
+    """
+    This is the main method to call.
+    """
     def run_game(self):
-        while True:
-            while(not self.game_is_over):
-                print(f'gamestate:{self.game_is_over}')
-                self.check_winner(self.player_one)
-            if input()=='q':
-                break
+        self.update_game_stats()
+        while not self.is_active_player_winning():
+            player = self.get_next_player()
+            player.doMove()
+            self.update_game_stats()
+        print(f"Congrats. {player} won the game.")
