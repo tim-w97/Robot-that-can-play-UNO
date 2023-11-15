c0a53d5de7b44cce9457d9394315f8f92d8e9ea0
Tim <timwagner997@gmail.com>
2023-11-15 17:14:40 +0100

    added a function to pick up card 1 to 6

--- robot/card_picker.py
+++ robot/card_picker.py
@@ -22,31 +22,48 @@ print("init")
 proxy = RobotProxy()
 proxy.connect()
 
-# prepare to pick up a card
-proxy.move(
-    x=x_pos_prepare,
-    y=y_pos_min,
-    z=z_pos,  # replace with real value
-    roll=rotation_90,
-    pitch=pickup_pitch,
-    yaw=rotation_90
-)
 
-# pickup card number 3
-proxy.move(
-    x=x_pos_max,
-    y=y_pos_min,
-    z=z_pos,  # replace with real value
-    roll=rotation_90,
-    pitch=pickup_pitch,
-    yaw=rotation_90
-)
+def pick_up_card(card_number):
+    if card_number not in range(1, 7):
+        raise ValueError(f"card number {card_number} doesn't exist.")
 
-proxy.grab()
+    x_pos = x_pos_min
+    y_pos = y_pos_min
+    yaw = rotation_90
 
-proxy.moveToHomePose()
-proxy.release()
+    if card_number > 3:
+        y_pos = y_pos_max
+        yaw = -rotation_90
+
+    if card_number % 3 == 2:
+        x_pos = x_pos_middle
+
+    if card_number % 3 == 0:
+        x_pos = x_pos_max
+
+    # prepare to pick up a card
+    proxy.move(
+        x=x_pos_prepare,
+        y=y_pos,
+        z=z_pos,  # replace with real value
+        roll=rotation_90,
+        pitch=pickup_pitch,
+        yaw=yaw
+    )
+
+    # pick up card with given card_number
+    proxy.move(
+        x=x_pos,
+        y=y_pos,
+        z=z_pos,  # replace with real value
+        roll=rotation_90,
+        pitch=pickup_pitch,
+        yaw=yaw
+    )
+
+
+# test the card pick up function
+pick_up_card(6)
 
 # disconnecting the robot
 proxy.disconnect()
-
