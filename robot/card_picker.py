f208abbd255f0a8d511c00e63ad972629c2c433c
Tim <timwagner997@gmail.com>
2023-11-15 18:47:30 +0100

    adjusted values for movement

--- robot/card_picker.py
+++ robot/card_picker.py
@@ -5,16 +5,17 @@ from control import RobotProxy
 pickup_pitch = math.radians(25)
 rotation_90 = math.radians(90)
 
-x_pos_prepare = 0.16
+x_pos_prepare = 0.12
 
-x_pos_min = 0.24
-x_pos_max = 0.4
+x_pos_min = 0.2
+x_pos_max = 0.35
 x_pos_middle = (x_pos_min + x_pos_max) / 2
 
 y_pos_min = -0.17
 y_pos_max = 0.17
 
 z_pos = 0.065
+z_pos_up = 0.2
 
 # setting up the robot
 print("init")
@@ -41,11 +42,14 @@ def pick_up_card(card_number):
     if card_number % 3 == 0:
         x_pos = x_pos_max
 
+    # open the grabber in case it's closed
+    proxy.release()
+
     # prepare to pick up a card
     proxy.move(
         x=x_pos_prepare,
         y=y_pos,
-        z=z_pos,  # replace with real value
+        z=z_pos,
         roll=rotation_90,
         pitch=pickup_pitch,
         yaw=yaw
@@ -55,15 +59,34 @@ def pick_up_card(card_number):
     proxy.move(
         x=x_pos,
         y=y_pos,
-        z=z_pos,  # replace with real value
+        z=z_pos,
         roll=rotation_90,
         pitch=pickup_pitch,
         yaw=yaw
     )
 
+    proxy.grab()
 
-# test the card pick up function
-pick_up_card(6)
+    # move card up
+    proxy.move(
+        x=x_pos,
+        y=y_pos,
+        z=z_pos_up,
+        roll=rotation_90,
+        pitch=pickup_pitch,
+        yaw=yaw
+    )
+
+    # just for fun
+    proxy.moveJoints(1, -0.3, 1, 0, 0, 0)
+
+    proxy.release()
+
+    proxy.moveToHomePose()
+
+
+for i in range(1,7):
+    pick_up_card(i)
 
 # disconnecting the robot
 proxy.disconnect()
