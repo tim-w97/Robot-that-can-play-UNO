b0ab518265ddcd79f297ccd4ac2780c2455711f2
Lukas Drescher <lukas.drescher@hof-university.de>
2023-12-01 11:11:48 +0100

    Added initial method

--- main.py
+++ main.py
@@ -1,42 +1,89 @@
-import cv2 as cv
-import color_detector
-import detection
-import card_cropper
+from ultralytics import YOLO
+from image_transformer import transform_image
+from color_detector import determine_color
+
+import cv2
 import config
+from game_classes import *
+from robotPlayer import RobotPlayer
 
-capture = cv.VideoCapture(config.camera_index)
+import numpy
 
-if not capture.isOpened():
-    print('Cannot open camera. Did you set the right camera index in config.py?')
-    exit()
 
-while True:
-    ret, frame = capture.read()
+"""
+TODO:
+- Import the model
+- Init the RobotPlayer
+- Init the HumanPlayer
+- Analyze the initCard
+- Create the game instance
+- Run the game
+"""
 
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+# init the model
+model = YOLO(config.model_path)
 
-    cropped_cards = card_cropper.crop_cards_from_img(frame)
+# init the player
+initialCard = UnoCard(3, Color.BLUE)
+robotPlayer = RobotPlayer("Rob", model)
+player = HumanPlayer("Lukas")
 
-    if cropped_cards is None:
-        continue
-    
-    for cropped_card in cropped_cards:
-        cv.imshow('frame', cropped_card)
+# run the game
+game = Game(robotPlayer, player, initialCard=initialCard)
+game.run_game()
 
-        # Detect color for the card
-        color = color_detector.detect_color(cropped_card)
-        print(f'Color of this card is {color}')
+# cleanup
+cv2.destroyAllWindows()
 
-        # Detect number for the card
-        number = detection.process_uno_card(cropped_card)  # Assuming cropped_card is a valid image array
-        print(f'Number on this card is {number}')
+def getInitCard() -> UnoCard:
+    cap = cv2.VideoCapture(config.camera_index)
+    ret, image = cap.read()
 
-    cv.imshow('frame', cropped_cards[0])
+    transformed_image = transform_image(image)
+    results = model(transformed_image)
 
-    if cv.waitKey(1) == ord('q'):
-        break
+    if len(results) > 0:
+        first_result = results[0]
 
-capture.release()
-cv.destroyAllWindows()
+        plot = first_result.plot()
+
+        for box in first_result.boxes:
+            # get coordinates of the bounding box
+            # xyxy means x1 and y1 of the top left corner and x2 and y2 of the bottom right corner
+            bounding_boxes = box.xyxy.tolist()
+
+            # determine the position of the pixel on the center right of the bounding box
+
+            if len(bounding_boxes) == 0:
+                continue
+
+            bounding_box = bounding_boxes[0]
+
+            center_right = (
+                int(bounding_box[2]),
+                int((bounding_box[1] + bounding_box[3]) / 2)
+            )
+
+            color_bgr = transformed_image[
+                center_right[1],
+                center_right[0]
+            ]
+
+            color = determine_color(color_bgr)
+
+            cv2.putText(
+                plot,
+                color.name,
+                center_right,
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 0, 0),
+                2
+            )
+
+        cv2.imshow('Detected Uno Cards', plot)
+    else:
+        cv2.imshow('No Objects', transformed_image)
+
+    cap.release()
+
