fee35b6b135f4ccdc43567f280d386e77b6be5dc
Tim <timwagner997@gmail.com>
2023-12-01 11:57:33 +0100

    refactored detector to one simple method

--- detector.py
+++ detector.py
@@ -4,83 +4,70 @@ from color_detector import determine_color
 
 import cv2
 import config
-
 import numpy
 
+"""
+This methods predicts all uno cards from the given image
 
-# fix deprecation issue
-def patch_asscalar(a):
-    return a.item()
+1. Transform the given image so the number detection works better
+2. Predict all uno cards and their corresponding positions (1-6)
+3. Return an array of tuples (Uno Card, Position)
+"""
+def predict_uno_cards(image):
+    # ignore this code (start)
+    # but leave it here so the color detector works
+    def patch(a):
+        return a.item()
 
+    setattr(numpy, "asscalar", patch)
 
-setattr(numpy, "asscalar", patch_asscalar)
+    # ignore this code (end)
 
-# Load a model
-model = YOLO(config.model_path)
-
-fake_image = None
-cap = None
-
-if config.use_fake_image:
-    fake_image = cv2.imread(config.fake_image_path)
-else:
-    cap = cv2.VideoCapture(0)
-
-while True:
-    if fake_image is not None:
-        image = fake_image
-    else:
-        ret, image = cap.read()
+    # Load a model and the card numbers (classes)
+    model = YOLO(config.model_path)
+    card_numbers = model.names
 
     transformed_image = transform_image(image)
+
+    # predict all uno cards from the image
     results = model(transformed_image)
 
-    if len(results) > 0:
-        first_result = results[0]
+    if len(results) == 0:
+        return []
 
-        plot = first_result.plot()
+    first_result = results[0]
 
-        for box in first_result.boxes:
-            # get coordinates of the bounding box
-            # xyxy means x1 and y1 of the top left corner and x2 and y2 of the bottom right corner
-            bounding_boxes = box.xyxy.tolist()
+    for box in first_result.boxes:
+        # get the predicted card number
+        predicted_class = int(box.cls)
+        card_number = card_numbers[predicted_class]
 
-            # determine the position of the pixel on the center right of the bounding box
+        # get coordinates of the bounding box
+        # xyxy means x1 and y1 of the top left corner and x2 and y2 of the bottom right corner
+        bounding_boxes = box.xyxy.tolist()
 
-            if len(bounding_boxes) == 0:
-                continue
+        if len(bounding_boxes) == 0:
+            continue
 
-            bounding_box = bounding_boxes[0]
+        bounding_box = bounding_boxes[0]
 
-            center_right = (
-                int(bounding_box[2]),
-                int((bounding_box[1] + bounding_box[3]) / 2)
-            )
+        center_right = (
+            int(bounding_box[2]),
+            int((bounding_box[1] + bounding_box[3]) / 2)
+        )
 
-            color_bgr = transformed_image[
-                center_right[1],
-                center_right[0]
-            ]
+        color_bgr = transformed_image[
+            center_right[1],
+            center_right[0]
+        ]
 
-            color = determine_color(color_bgr)
+        color = determine_color(color_bgr)
 
-            cv2.putText(
-                plot,
-                color.name,
-                center_right,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 0, 0),
-                2
-            )
+        print(card_number)
+        print(color)
 
-        cv2.imshow('Detected Uno Cards', plot)
-    else:
-        cv2.imshow('No Objects', transformed_image)
 
-    # break the loop if the user presses escape key
-    if cv2.waitKey(1) == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+# test the method
+predict_uno_cards(
+    image=cv2.imread('uno cards test image.jpeg')
+)
